9fcd259dddd70833d74addf084788f4a75ad3f93
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-09-27 18:38:29 +0300

    Add files via upload

---
+++ ConsoleApplicationEx8.cpp
@@ -0,0 +1,59 @@
+﻿// ConsoleApplicationEx8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b;
+	char d;
+
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите данные";
+	cin >> a >> d >> b;
+	switch (d)
+	{
+	case'+':
+		cout << a + b;
+		break;
+	case'-':
+		cout << a - b;
+		break;
+	case'*':
+		cout << a * b;
+		break;
+	case '/':
+		if (b==0)
+		{
+			cout << "Некорретные данные деление на 0 невозможно";
+		}
+		else
+		{
+			cout << a / b;
+		}
+		break;
+	default:
+		cout<<"Некорретные данные, уточните арифметическое действие";
+		break;
+	}
+
+	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
+
