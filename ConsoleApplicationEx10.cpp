1a5dbcb869402b9a7e1d64922381bd5126ef76c6
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-09-27 19:27:32 +0300

    Add files via upload

---
+++ ConsoleApplicationEx10.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <clocale>
+#include <time.h>
+using namespace std;
+
+
+
+double exponentiation(double a, int b, double result = 1)
+{
+	if (b == 0) {
+		return result;
+	}
+	else if (b > 0) {
+		result *= a;
+		return exponentiation(a, b - 1, result);
+	}
+	else {
+		result /= a;
+		return exponentiation(a, b + 1, result);
+	}
+}
+
+
+
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	
+		double a;
+		int b;
+
+		cin >> a;
+		cin >> b;
+
+		cout << exponentiation(a, b);
+
+		
+
+	return 0;
+}
+
+
+
+
