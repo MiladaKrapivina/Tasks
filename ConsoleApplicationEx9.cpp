d99de2f5f7a256b96c1c00e422af2ace97e7ce25
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-09-27 18:37:44 +0300

    Add files via upload

---
+++ ConsoleApplicationEx9.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <clocale>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	cout<<"Игра \"Угадай число\" "<<endl;
+	srand(time(NULL));
+	int num, n, v;
+	do {
+		num = rand() % 101;
+		int i = 0;
+		bool flag = false;
+		do {
+			if (i == 5) {
+				flag = true; break;
+			}
+			cout << "Ваше число" << endl;
+			cin >> n;
+			if (n == num) {
+				cout << "вы угадали!!!" << endl;
+				break;
+			}
+			else if (n < num) {
+				cout << "Загаданное число больше"<<endl;
+			}
+			else {
+				cout << "Загаданное число меньше"<<endl;
+			}
+			i++;
+
+		} while (!flag );
+		cout << "Хотите сыграть ещё? Нажмите 1 если да";
+		cin >> v;
+
+	} while (v==1);
+
+
+	return 0;
+}
+
+
+
+
