aa382cbc359a3e5cd2e8d6633ae8692c7b8d7300
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-09-29 20:08:26 +0300

    Add files via upload

---
+++ ConsoleApplicationEx13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cout << "Введите число" << endl;
+	cin >> a;
+	int A = 0;
+	int temp;
+	for (int i = 1; i <= a; i++) {
+		temp = log(i) / log(2);
+		if (i - pow(2, temp) == 0) A++;
+	}
+
+	cout << A;
+
+	return 0;
+}
