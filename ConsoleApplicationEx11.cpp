f4e0da3a336830ba974d06b6a8c3ca078ecbbf23
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-09-29 19:19:20 +0300

    Add files via upload

---
+++ ConsoleApplicationEx11.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Введите проверяемое число" << endl;
+	cin >> n;
+	for (i = 2; i < sqrt(n); i++) {
+		if (n % i == 0) {
+			cout << "Данное число не является простым";
+			return 0;
+		}
+	}
+	cout << "Данное число не является простым";
+	return 0;
+}
---
+++ ConsoleApplicationEx12.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i;
+	cout << "Введите проверяемое число" << endl;
+	cin >> n;
+	for (i = 2; i < sqrt(n); i++) {
+		if (n % i == 0) {
+			cout << "Данное число не является простым";
+			return 0;
+		}
+	}
+	cout << "Данное число  является простым";
+	return 0;
+}
