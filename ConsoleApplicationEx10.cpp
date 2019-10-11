76a1c3f15f2911a92dd65c4c428ea0aba9198d88
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-10-11 23:12:26 +0300

    Update ConsoleApplicationEx10.cpp

--- ConsoleApplicationEx10.cpp
+++ ConsoleApplicationEx10.cpp
@@ -1,20 +1,20 @@
-﻿#include <iostream>
+#include <iostream>
 #include <clocale>
 #include <time.h>
 using namespace std;
 
-double exponentiation(double a, int b, double result = 1)
+double ex(double a, int b, double result = 1)
 {
 	if (b == 0) {
 		return result;
 	}
 	else if (b > 0) {
 		result *= a;
-		return exponentiation(a, b - 1, result);
+		return ex(a, b - 1, result);
 	}
 	else {
 		result /= a;
-		return exponentiation(a, b + 1, result);
+		return ex(a, b + 1, result);
 	}
 }
 
@@ -22,18 +22,37 @@ double exponentiation(double a, int b, double result = 1)
 
 int main()
 {
-	setlocale(LC_ALL,"Russian");
-	
-		double a;
-		int b;
+	setlocale(LC_ALL, "Russian");
 
-		cin >> a;
-		cin >> b;
+	double a;
+	int b;
+	cout << "a=";
+	cin >> a;
+	cout << "b=";
+	cin >> b;
 
-		cout << exponentiation(a, b);
-
-		
-
+	if (a==0)
+	{
+		if (b==0)
+		{
+			cout << "Неопределенно ";
+			return 0;
+		}
+		if (b>0)
+		{
+			cout << 0;
+			return 0;
+		}
+		if (b<0)
+		{
+			cout << "Ошибка: делить на 0 нельзя";
+			return 0;
+		}
+	}
+	else
+	{
+		cout << ex(a, b);
+	}
 	return 0;
 }
 
