b864e1d8d5a2dd95eb132e27b454af5619f4ca92
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-10-15 21:20:54 +0300

    Update ConsoleApplicationEx10.cpp

--- ConsoleApplicationEx10.cpp
+++ ConsoleApplicationEx10.cpp
@@ -3,47 +3,29 @@
 #include <time.h>
 using namespace std;
 
-double ex(double a, int b, double result = 1)
-{
-	if (b == 0) {
-		return result;
-	}
-	else if (b > 0) {
-		result *= a;
-		return ex(a, b - 1, result);
-	}
-	else {
-		result /= a;
-		return ex(a, b + 1, result);
-	}
-}
-
-
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-
-	double a;
+	double a,res=1;
 	int b;
 	cout << "a=";
 	cin >> a;
 	cout << "b=";
 	cin >> b;
 
-	if (a==0)
+	if (a == 0)
 	{
-		if (b==0)
+		if (b == 0)
 		{
-			cout << "Неопределенно ";
+			cout << 1;
 			return 0;
 		}
-		if (b>0)
+		if (b > 0)
 		{
 			cout << 0;
 			return 0;
 		}
-		if (b<0)
+		if (b < 0)
 		{
 			cout << "Ошибка: делить на 0 нельзя";
 			return 0;
@@ -51,8 +33,18 @@ int main()
 	}
 	else
 	{
-		cout << ex(a, b);
+		if (b > 0) {
+			for (int i = 1; i <= b; i++) {
+				res *= a;
+            }
+		}
+		else {
+			for (int i = 1; i <= -b; i++) {
+				res /= a;
+			}
+		}
 	}
+	cout << res;
 	return 0;
 }
 
