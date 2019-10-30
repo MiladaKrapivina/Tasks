60156735fd86d876325dc0664caddbd9230d6ca0
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-10-30 14:25:04 +0300

    Update ConsoleApplicationEx10.cpp

--- ConsoleApplicationEx10.cpp
+++ ConsoleApplicationEx10.cpp
@@ -18,17 +18,14 @@ int main()
 		if (b == 0)
 		{
 			cout << 1;
-			return 0;
 		}
 		if (b > 0)
 		{
 			cout << 0;
-			return 0;
 		}
 		if (b < 0)
 		{
 			cout << "Ошибка: делить на 0 нельзя";
-			return 0;
 		}
 	}
 	else
@@ -37,8 +34,11 @@ int main()
 			for (int i = 1; i <= b; i++) {
 				res *= a;
             }
-		}
-		else {
+		} 
+		
+		else 
+			if (b = 0) cout << 1;
+		 else {
 			for (int i = 1; i <= -b; i++) {
 				res /= a;
 			}
