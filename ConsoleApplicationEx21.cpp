2ab32b4f16890c706f5cdfda25c381bd0ee9ddce
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-11-13 21:24:48 +0300

    Update ConsoleApplicationEx21.cpp

--- ConsoleApplicationEx21.cpp
+++ ConsoleApplicationEx21.cpp
@@ -1,49 +1,43 @@
+
 #include <iostream>
 #include <time.h>
 using namespace std;
 int main(){
 	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
-	int arr[10][8]{ 0 };
 	int m, n, V;
 	int N, M;
 	do {
-		m = rand() % 8;
-		n = rand() % 10;
+		int arr[10][8]{ 0 };
+		n = rand() % 8;
+		m = rand() % 10;
 		arr[m][n] = 1;
 		int i = 0;
-		while (true){
+		while (true) {
 			cout << "Введите значения" << endl;
 			cin >> N >> M;
-			int zn[5];
-			bool povt = false;
-			zn[i] = 10 * N + M;
-			if (i > 0) {
-				for (int j = 0; j < 2; j++) {
-					if (zn[j] == zn[j + 1] || zn[j] == zn[j + 2]||zn[j] == zn[j + 3]  )
-					{
-						povt = true;
-					}
-				}
-			}
-			if(i == 4) {
-				cout << "Попыток не осталось.Вы проиграли."<<endl;
+
+			if (i == 4) {
+				cout << "Попыток не осталось.Вы проиграли." << endl;
 				break;
 			}
-			if (N == n && M == m) {
+			if (arr[M][N] == 1) {
 				cout << "Вы угадали";
 				break;
 			}
-			else if (N >= 10 || M >= 8) {
+			else if (N >= 10 || M >= 8 || N < 0 || M < 0) {
 				cout << "Вы ввели недопустимые координаты ячейки" << endl;
-				--i;
+				
 			}
-			else if (povt) {
+			else if (arr[M][N] == 2) {
 				cout << "Вы уже вводили эти координаты " << endl;
-				--i;
+				
 			}
-			else cout << "Неверные  координаты ";
-			i++;
+			else {
+				cout << "Неверные  координаты ";
+				arr[M][N] = 2;
+				i++;
+			}
 		}
 		cout << " Хотите сыграть ещё? Нажмите 1, если да.   ";
 		cin >> V;
