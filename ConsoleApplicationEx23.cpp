0a72ab1eff061cb69ef8cff3674ab96eebdce8ea
MiladaKrapivina <55448714+MiladaKrapivina@users.noreply.github.com>
2019-11-26 12:51:48 +0300

    Update ConsoleApplicationEx23.cpp

--- ConsoleApplicationEx23.cpp
+++ ConsoleApplicationEx23.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
- bool proverka(char s1[]) {
+ bool proverka(char s1[], int len1) {
 	string s;
-	for (int n = 0; n < sizeof(s1); n++) {
+	for (int n = 0; n < len1; n++) {
 			if (isdigit(s1[n])) {
 				s += s1[n];
 			}
@@ -22,10 +22,11 @@ using namespace std;
 }
 int main() {
 	setlocale(LC_ALL, "Russian");
-	char s1 [20];
+	char s1 [21];
 	cout << "Введите проверяемую строку";
 	cin.getline(s1, 20);
-	if (proverka(s1)) {
+	int len1=sizeof(s1);
+	if (proverka(s1, len1)) {
 		cout << "Палиндром";
 	}else  cout << "Не палиндром";
 	return 0;
